93eb0a8767cc587d2f9e553edc896662b665b680
THe-L00t(EPC) <sigun2001@naver.com>
2025-11-17 13:45:20 +0900

    shader클래스 생성

---
+++ homework/headHeader.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <functional>
+#include <optional>
+#include <sstream>
+#include <filesystem>
+#include <gl/glew.h> 
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+
+#include <gl/glm/glm.hpp>
+#include <gl/glm/ext.hpp>
+#include <gl/glm/gtc/matrix_transform.hpp>
---
+++ homework/main.cpp
@@ -0,0 +1,192 @@
+﻿#include "loadShader.hpp"
+#include "headheader.h"
+
+namespace beginConfig {
+	int width{ 800 };
+	int height{ 800 };
+	Color bg{ 1,1,1,1 };
+}
+namespace CameraConfig {
+	glm::vec3 pos{ 5.f,5.f,-2.f };
+	glm::vec3 dir{ 0,0,0 };
+	glm::vec3 up{ 0,1,0 };
+}
+
+namespace ViewfConfig {
+	float fovy{ glm::radians(45.f) };
+	float aspect{ 1.f };
+	float n{ 1.f };
+	float f{ 100.f };
+}
+
+GameTimer* GameTimer::Instance;
+
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+GLvoid Keyboard(unsigned char key, int x, int y);
+GLvoid RanColor(Color&);
+GLvoid Mouse(int, int, int, int);
+GLvoid loop(int);
+
+
+GLuint CompileShaders(const std::string_view&, const std::string_view&);
+bool ReadFile(const std::string_view&, std::string*);
+void AddShader(GLuint, const std::string_view&, GLenum);
+
+// Shader 
+GLuint bs{};
+GLuint VBO{};
+std::vector<float> Vertex{};
+
+GameTimer gt;
+
+int num{};
+int method{};
+Color bg = beginConfig::bg;
+
+
+void main(int argc, char** argv)
+{
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(beginConfig::width, beginConfig::height);
+	glutCreateWindow("Example1");
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Unable to initialize GLEW" << std::endl
+			;
+		exit(EXIT_FAILURE);
+	}
+	else {
+		std::cout << "GLEW Initialized\n";
+	}
+	bs = CompileShaders("CG1-15.vs", "CG1-15.fs");
+
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(Reshape);
+	glutKeyboardFunc(Keyboard);
+	glutMouseFunc(Mouse);
+	glutTimerFunc(1, loop, 1);
+	glutMainLoop();
+}
+
+GLvoid drawScene()
+{
+
+	glClearColor(bg.r, bg.g, bg.b, bg.al);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	// 셰이더 사용하여 그리기
+	GLuint shader = bs;
+	glUseProgram(shader);
+
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	glutSwapBuffers();
+}
+
+GLvoid Reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+}
+
+
+GLvoid Keyboard(unsigned char key, int x, int y)
+{
+
+	switch (key) {
+	case '1':
+
+		break;
+	case '2':
+
+		break;
+	case '3':
+
+		break;
+	case '4':
+
+		break;
+	case '5':
+
+		break;
+	case '6':
+
+		break;
+	case '7':
+
+		break;
+	case '8':
+
+		break;
+	case '9':
+
+		break;
+	case '0':
+
+		break;
+	case 'c':
+
+		break;
+	case 't':
+
+		break;
+	case 'q':
+		glutLeaveMainLoop();
+		break;
+	}
+	glutPostRedisplay();
+}
+
+//GLvoid RanColor(Color& c)
+//{
+//	float r = urgb(dre);
+//	float g = urgb(dre);
+//	float b = urgb(dre);
+//	c = { r / 255,g / 255,b / 255,1 };
+//}
+
+GLvoid Mouse(int button, int state, int x, int y)
+{
+	int once{ 0 };
+	float mpx{ (static_cast<float>(x) - 400) / 400 }, mpy{ (static_cast<float>(-y) + 400) / 400 };
+	if (state == GLUT_DOWN) {
+		if (once == 0) {
+
+
+			once = 1;
+		}
+	}
+	if (state == GLUT_UP) {
+		std::cout << "upȣ��" << std::endl;
+		once = 0;
+	}
+	switch (button) {
+	case GLUT_LEFT_BUTTON:
+		if (once == 1) {
+			once = 2;
+		}
+		break;
+	case GLUT_MIDDLE_BUTTON:
+		break;
+	case GLUT_RIGHT_BUTTON:
+
+		break;
+	}
+
+
+}o
+
+GLvoid loop(int v)
+{
+	gt.Update();
+
+
+
+	glutPostRedisplay();
+	glutTimerFunc(1, loop, 1);
+}
